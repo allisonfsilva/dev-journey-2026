68fd0c47f13d22d961a59ef61e5d4cf2aa6e9fbb
Allison Silva <b4zz1ng4@gmail.com>
2026-08-11 09:20:46 -0300

    primeiroupt

---
+++ java-básico/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    // psvm -> public static void main
+    // sout -> System.out.println
+    
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
